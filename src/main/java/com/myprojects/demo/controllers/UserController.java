ba32ed9448b9d0d784a269d1c16a742a1adc2e25
pnikolidakis <pnikolidakis@procureship.com>
2023-11-12 18:13:00 +0200

    add controller methods

--- src/main/java/com/myprojects/demo/controllers/UserController.java
+++ src/main/java/com/myprojects/demo/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.myprojects.demo.controllers;
 
+import com.myprojects.demo.dto.RoleToUserForm;
 import com.myprojects.demo.dto.UserForm;
 import com.myprojects.demo.entities.MovieUser;
+import com.myprojects.demo.entities.Role;
 import com.myprojects.demo.services.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,8 +22,26 @@ public class UserController {
     }
 
     @GetMapping("/all")
-    public List<MovieUser> getAllUsers() {
-        return userService.getAllUsers();
+    public ResponseEntity<List<MovieUser>> getAllUsers() {
+        return ResponseEntity.ok().body(userService.getAllUsers());
+    }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<MovieUser> saveUser(@RequestBody MovieUser user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.addUser(user.getUsername(), user.getPassword()));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addToUser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form) {
+        userService.addRoleToUser(form.getUsername(), form.getRoleName());
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/new-user")
