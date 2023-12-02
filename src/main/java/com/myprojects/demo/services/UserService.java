1dc8f26af310b5158adb0d7244e620bc59d82eb7
pnikolidakis <pnikolidakis@procureship.com>
2023-12-02 14:31:48 +0200

    userservice extends UserDetailsService

--- src/main/java/com/myprojects/demo/services/UserService.java
+++ src/main/java/com/myprojects/demo/services/UserService.java
@@ -2,10 +2,11 @@ package com.myprojects.demo.services;
 
 import com.myprojects.demo.entities.MovieUser;
 import com.myprojects.demo.entities.Role;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     List<MovieUser> getAllUsers();
     MovieUser getUser(String username);
     MovieUser addUser(String username, String password);
