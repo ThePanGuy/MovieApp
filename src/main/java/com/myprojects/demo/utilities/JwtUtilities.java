ee331aec3302acbc561fd52adbf2759a8efda037
pnikolidakis <pnikolidakis@procureship.com>
2023-12-02 14:27:52 +0200

    change jwt utilities to accept userDetails

--- src/main/java/com/myprojects/demo/utilities/JwtUtilities.java
+++ src/main/java/com/myprojects/demo/utilities/JwtUtilities.java
@@ -6,7 +6,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.myprojects.demo.dto.DecodedData;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.*;
 
@@ -38,7 +38,7 @@ public class JwtUtilities {
                 .sign(algorithm);
     }
 
-    public static String createRefreshToken(User user, String issuer) {
+    public static String createRefreshToken(UserDetails user, String issuer) {
         Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
         return JWT.create()
                 .withSubject(user.getUsername())
